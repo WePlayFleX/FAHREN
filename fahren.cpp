5d0c9a2a2136c1bf4a8fe753c890be2fa5622c76
WePlayFleX <alex.kamai@ya.ru>
2021-11-28 23:20:56 +0300

    Добавьте файлы проекта.

---
+++ fahren.cpp
@@ -0,0 +1,17 @@
+﻿//fahren.cpp
+//применение cin и \n
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ftemp;		//температура по фаренгейту
+	cout << "Enter the temperature in Fahrenheit: ";
+	cin >> ftemp;
+	int ctemp = (ftemp - 32) * 5 / 9;
+	cout << "The Celsius temperature is " << ctemp << '\n';
+
+	system("pause");
+	return 0;
+}
